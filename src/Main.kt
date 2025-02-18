4c2b06f66061dbb99bbb65469025b5a1088db85f
Taine <tcsmith2@waimea.school.nz>
2025-02-18 13:22:31 +1300

    working guessing game

--- src/Main.kt
+++ src/Main.kt
@@ -1,87 +1,84 @@
 /**
- *a little guessing game in Kotlin
+ * A little guessing game in Kotlin
  */
 
-
-
-
 fun main() {
-    println("________________________________________________")
-    println(" GUESSING GAME ")
-    println("________________________________________________")
+    println("----------------")
+    println(" Guessing game 5 Million")
+    println("----------------")
     println()
 
-
-    val playername = getString("enter yourtname: ")
-    println ("Nice to meet you, $playername")
+    val playerName = getString("Enter your name: ")
+    println("Nice to meet you $playerName")
 
 
     val numToGuess = (1..100).random()
 
+    println("I'm thinking of a number between 1 and 100. Can you guess it?")
 
 
+    //Main loop
+    while (true) {
 
+        //Ask the user for a guess
+        println("Have a guess: ")
+        val guess = readln().toIntOrNull()
+        //Check if their guess is correct
+        if (guess == null) {
+            println("I said a number!")
+            continue
+        }
+        if (guess == numToGuess) {
+            println("Well done! You got it.")
+            break
+        }
 
-//ask the user for the guess
-//check if it's correct or not
-//break out of loop if correct
-//Otherwise give high/lower feedback
-
-
-    println("Im thinking of a number from 1 to 100")
-    println("Can you guess it?")
-
-        if (guess == null)
-    print("Enter your  number: ")
-    val number = readln().toIntOrNull()
-    if (number == null) {
+        if (guess < numToGuess) {
+            println("Too low")
+        } else {
+            println("Too high")
+        }
 
 
     }
+//Break out of loop if correct
 
+    //Otherwise give higher/lower feedback
 
-    /**
-     * function to get a string from user
-     * parameters
-     *- prompt - Text to show the user
-     * returns
-     * - string that user types
-     */
-    fun getString(prompt: String): String {
-        while (true)
-            print(prompt)
-        val userInput: string
-            val userInput = readln()
-            if (userInput.isNotBlank()) break
+}
+/**
+ * Function to get a string from a user
+ * parameters:
+ * - prompt - text to show the user
+ * returns:
+ * - String that the user types
+ */
+fun getString(prompt: String): String {
+    var userInput: String
+    while (true) {
+        print(prompt)
+        userInput = readln()
+        if (userInput.isNotBlank())
+            break
 
     }
     return userInput
-
 }
-
 /**
- * Function to get an Int from the user
+ * Function to get Int from user
  */
-    fun getInt(prompt: String): Int {
-        while (true) {
+fun getInt(prompt: String): Int {
+    var intValue: Int?
+
+    while (true) {
         val userInput = getString(prompt)
-        val intValue = userInput.toIntOrnull()
+        intValue = userInput.toIntOrNull()
         if (intValue != null) break
-
-        }
-        return intValue
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
+    return intValue!!
 }
+
+
+
+
+
